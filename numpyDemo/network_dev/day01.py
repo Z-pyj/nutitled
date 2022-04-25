04ccdf5dad80fd134891616b0502315be89f54b4
zw <pyj.moon@gmail.com>
2022-04-26 00:05:29 +0800

    urllib

--- numpyDemo/network_dev/day01.py
+++ numpyDemo/network_dev/day01.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     url = 'https://python.org'
     # urlopen()请求网址
     response = urllib.request.urlopen(url)
+    # 获取响应内容
+    print(response.read().decode('utf-8'))
     # 响应类型为<class 'http.client.HTTPResponse'>
     print(type(response))
     # status属性获取响应码
