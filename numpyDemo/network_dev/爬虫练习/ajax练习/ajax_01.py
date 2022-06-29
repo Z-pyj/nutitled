942b773665829a124ae4cbf41e3b4e395da1bb4c
zw <pyj.moon@gmail.com>
2022-06-29 21:06:54 +0800

    ajax2

--- numpyDemo/network_dev/爬虫练习/ajax练习/ajax_01.py
+++ numpyDemo/network_dev/爬虫练习/ajax练习/ajax_01.py
@@ -1,10 +1,16 @@
+import pymongo
 import requests
 import logging
-
+import time
 LIMIT = 10
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 INDEX_URL = 'https://spa1.scrape.center/api/movie/?limit={limit}&offset={offset}'
 DETAIL_URL = 'https://spa1.scrape.center/api/movie/{id}'
+MONGO_CONNECTION_STRING = 'mongodb://localhost:27017'
+MONGO_DB_NAME = 'movies'
+client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
+db = client['movies']
+collection = db['movies']
 
 
 # 通用接口
@@ -32,11 +38,22 @@ def scrape_detail(id):
     return scrape_api(url)
 
 
+def save_data(data):
+    collection.update_one({
+        'name': data.get('name')
+    }, {
+        '$set': data
+    }, upsert=True)
+
+
 if __name__ == '__main__':
-    TOTAL_PAGE = 1
+    TOTAL_PAGE = 10
     for page in range(1, TOTAL_PAGE + 1):
+        time.sleep(3)
         index_data = scrape_index(page)
         for item in index_data.get('results'):
             scrape_id = item.get('id')
             detail_data = scrape_detail(scrape_id)
             logging.info('detail data %s', detail_data)
+            save_data(detail_data)
+            logging.info('data saved successfully')
