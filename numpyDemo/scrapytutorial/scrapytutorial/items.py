651e28b984a129811af7e02cf956d157674f9ecb
zw <pyj.moon@gmail.com>
2022-07-14 19:56:22 +0800

    scrapy

--- numpyDemo/scrapytutorial/scrapytutorial/items.py
+++ numpyDemo/scrapytutorial/scrapytutorial/items.py
@@ -6,10 +6,10 @@
 import scrapy
 
 
-class ScrapytutorialItem(scrapy.Item):
+class QuoteItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     text = scrapy.Field()
     author = scrapy.Field()
     tags = scrapy.Field()
-    pass
+
